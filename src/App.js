f1d5f073d14681f8627c7d8076ed48b36c85884a
Scott <sahelgeson07@gmail.com>
2019-07-24 18:52:30 -0400

    Initial work for handling submit, changing expenses, and showing recent expenses. Need to fix inconsistent alignment

--- src/App.js
+++ src/App.js
@@ -5,20 +5,76 @@ class App extends Component{
   constructor(props) {
     super(props);
   
+    /* Format for expenses (used in recentExpenses): 
+      {
+        datetime,
+        amount,
+        category
+      }
+    */
+
     this.state = {
-      mostRecentCategory: 'Food',
-      mostRecentCategoryColor: 'Food'
+      amount: '',
+      category: 'Food',
+      recentExpenses: [],
     }
+
+    this.handleAmountChange = this.handleAmountChange.bind(this);
+    this.handleCategoryChange = this.handleCategoryChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleAmountChange(event) {
+    this.setState({amount: event.target.value});
+  }
+
+  handleCategoryChange(event) {
+    console.log('Category was changed to: ' + this.state.category);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (!this.state.amount) { return false; }  /* TODO change this to an error message */
+      
+    console.log('An expense was submitted: ' + this.state.amount);
+    
+    const datetime = new Date();
+
+    const {amount, category} = this.state;
+
+    console.log('datetime: ' + datetime);
+    console.log('amount: ' + amount);
+    console.log('category: ' + category);
+
+    const newExpense = {
+      datetime,
+      amount,
+      category
+    }
+
+    this.setState({
+      recentExpenses: [newExpense, ...this.state.recentExpenses]
+    })
+  }  
   
   render(){
+    const recentExpenses = this.state.recentExpenses;
+    const numberOfRecentShown = 10; 
+
     return (
       <div className="App">
-        <form>
+        <form 
+          onSubmit={this.handleSubmit}
+        >
+          {/* TODO add an onselect so it resets the value when a user taps into it? */}
           <input 
             className="input font-25 mhl mvm"
             type="number" 
             placeholder="$0.00" 
+            min="0.01" 
+            step="0.01"
+            onChange={this.handleAmountChange}
+            value={this.state.amount}
           />
           <label
             className="label mbs"
@@ -26,11 +82,11 @@ class App extends Component{
             Category
           </label>
 
-          {/* TODO This input needs to be styles with the color associated with the category */}
           <input 
             className="input font-25 mhl mbm"
             type="button" 
-            value={this.state.mostRecentCategory} 
+            onChange={this.handleCategoryChange}
+            value={this.state.category} 
           />
           <input 
             className="input font-25 mhl mvm"
@@ -38,6 +94,27 @@ class App extends Component{
             value="Save" 
           />
         </form>
+
+        {recentExpenses.length ? (
+          <ol className="card mhl mvm">
+            {recentExpenses.slice(0,numberOfRecentShown).map((expense, i) =>
+              <li key={i}>
+                <div>
+                  {expense.datetime.getTime()}
+                </div>
+                <div>
+                  {expense.amount}
+                </div>
+                <div>
+                  {expense.category}
+                </div>
+              </li>
+            )}
+          </ol>
+        ) : null}
+        {recentExpenses.length > numberOfRecentShown ? (
+          <div>See all expenses</div> 
+        ) : null}
       </div>
     );
   }

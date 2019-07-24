22d2eab831954af5747e5720647ba95938b156ad
Scott <sahelgeson07@gmail.com>
2019-07-24 16:36:13 -0400

    initial commit, focus on saving instead of design for right now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      mostRecentCategory: 'Food',
+      mostRecentCategoryColor: 'Food'
+    }
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <form>
+          <input 
+            className="input font-25 mhl mvm"
+            type="number" 
+            placeholder="$0.00" 
+          />
+          <label
+            className="label mbs"
+          >
+            Category
+          </label>
+
+          {/* TODO This input needs to be styles with the color associated with the category */}
+          <input 
+            className="input font-25 mhl mbm"
+            type="button" 
+            value={this.state.mostRecentCategory} 
+          />
+          <input 
+            className="input font-25 mhl mvm"
+            type="submit" 
+            value="Save" 
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;

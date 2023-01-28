69b69f443cd002d7d6dd1c829aa14e0b2dd5aa8f
Scott <sahelgeson07@gmail.com>
2023-01-28 14:10:18 -0500

    upgraded all packages, some cypress tests are failing but were verified manually

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { DirtSimpleHomePage } from "./routes/DirtSimpleHome/DirtSimpleHome";
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DirtSimpleHomePage />} />
+        <Route path="/app/expensetracker/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+);

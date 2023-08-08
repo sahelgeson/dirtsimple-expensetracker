b855a1ec2ef09bddbc822b9a51f735d540958755
Scott <sahelgeson07@gmail.com>
2023-08-08 08:46:51 -0400

    makes HomeForm a big easy to click button that focuses on amount first for input entry

--- src/routes/Home/HomeForm.tsx
+++ src/routes/Home/HomeForm.tsx
@@ -1,16 +1,17 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import cuid from 'cuid';
-import { Box, Select } from '@chakra-ui/react';
+import { Box, Card, Button, Select } from '@chakra-ui/react';
+import { useDisclosure, Collapse } from '@chakra-ui/react';
 
 import { useGlobalState } from 'contexts';
 import { formatDatetime, parseStoredAmount } from 'helpers';
-import { Button } from "@chakra-ui/react";
 import { NumberInput, NumberInputField } from '@chakra-ui/react';
 import { UNCATEGORIZED } from 'lib/constants';
 import { CategoryId, Dollar, EmptyString } from 'interfaces';
 
 export const HomeForm = () => {
   const { allCategories, addExpense } = useGlobalState();
+  const { isOpen, onToggle } = useDisclosure();
 
   /* TODO: change this eventually so user can set default category */
   //const defaultCategoryId = allCategories[0]?.id  || [];
@@ -23,7 +24,41 @@ export const HomeForm = () => {
     setCategoryId(allCategories[0]?.id);
   }, [allCategories]);
 
+  const handleSpecialCaseClick = () => {
+    /* 
+      if isSaved is true, we want user to be able to click anywhere in card
+      other than form controls, that is why button is not disabled on save
+      (click events don't propagate from disabled buttons)
+      This just bubbles up depending on whether expense was saved or not to
+      allow Card to open
+    */
+    return isSaved;
+  }
+
+  const handleOpenCollapsible = (event: React.MouseEvent<HTMLElement>) => {
+    // never collapse via interaction, only resets when user reloads/returns to page
+    // so only do one time
+
+    // click events need to just bubble up if open and unsaved
+    if (isOpen && !isSaved) return true;
+    const targetId = (event.target as HTMLElement)?.id; 
+
+    // reset and focus on amount if opening Card
+    if (targetId !== 'amount' && targetId !== 'category') {
+      if (!isOpen) {
+        onToggle();
+      } 
+      document.getElementById('amount')?.focus();
+    }
+
+    return false;
+  }
+
   const handleAmountChange = (valueAsNumber: number) => {
+    if (!isOpen) {
+      onToggle();
+    }
+
     if (Number.isNaN(valueAsNumber)) {
       setAmount('');
     } else {
@@ -33,6 +68,7 @@ export const HomeForm = () => {
 
   const handleCategoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setCategoryId(event.currentTarget.value);
+    setIsSaved(false);
   }
 
   const handleFocus = () => {
@@ -42,7 +78,6 @@ export const HomeForm = () => {
 
   const handleSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault();
-
     if (!amount) {  // don't submit on 0 or NaN or ''
       setIsSaved(false);
       return false; 
@@ -72,77 +107,88 @@ export const HomeForm = () => {
     return;
   }  
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label 
-        htmlFor="amount"
-        className="sr-only"            
-      >
-        Enter amount of this expense  
-      </label>
-
-      <NumberInput 
-        value={amount}
-        onChange={(_, valueAsNumber) => handleAmountChange(valueAsNumber)}
-        onFocus={handleFocus}        
-        size="xlg"
-        variant="darkerBorder"
-        mb={2}
-      >
-        <NumberInputField         
-          width={'100%'} 
-          placeholder="$0.00"
-          data-qa="main-form-amount-input" 
-        />
-      </NumberInput>
-
-      <Box mt={4} mb={2}>
-        <label
-          htmlFor="category"
-          className="block text-center gray-777 mbs"
+  return (    
+    <Card p={12} pt={24} pb={isSaved ? 12 : 24} sx={{ borderRadius: 110 }} onClick={handleOpenCollapsible}>
+      <form onSubmit={handleSubmit}>
+        <label 
+          htmlFor="amount"
+          className="sr-only"            
         >
-          Category
+          Enter amount of this expense  
         </label>
-      </Box>
 
-      <Select 
-        id="category"
-        placeholder="Select category"
-        size="xlg"
-        variant="darkerBorder"
-        mb={8}
-        value={categoryId} 
-        onChange={handleCategoryChange}
-        data-qa="main-form-category-input" 
-      >     
-        {allCategories
-          .filter((category) => {
-            return category.id !== UNCATEGORIZED
-          })
-          .map((category) => {
-            return (
-              <option 
-                key={category.id}
-                value={category.id}
-              >
-                {category.name}
-              </option>
+        <NumberInput 
+          id="amount"
+          value={amount}
+          onChange={(_, valueAsNumber) => handleAmountChange(valueAsNumber)}
+          onFocus={handleFocus}        
+          size="xlg"
+          variant="darkerBorder"
+          mb={2}
+        >
+          <NumberInputField         
+            width={'100%'} 
+            placeholder="$0.00"
+            data-qa="main-form-amount-input" 
+          />
+        </NumberInput>
+
+        <Box mt={4} mb={2}>
+          <label
+            htmlFor="category"
+            className="block text-center gray-777 mbs"
+          >
+            Category
+          </label>
+        </Box>
+
+        <Select 
+          id="category"
+          placeholder="Select category"
+          size="xlg"
+          variant="darkerBorder"
+          value={categoryId} 
+          onChange={handleCategoryChange}
+          data-qa="main-form-category-input" 
+        >     
+          {allCategories
+            .filter((category) => {
+              return category.id !== UNCATEGORIZED
+            })
+            .map((category) => {
+              return (
+                <option 
+                  key={category.id}
+                  value={category.id}
+                >
+                  {category.name}
+                </option>
+            )}
           )}
-        )}
-      </Select>
+        </Select>
 
-      <Button 
-        size="xlg"
-        type="submit"
-        variant={isSaved ? 'success' : 'solid'}
-        isDisabled={!amount}   
-        width="100%"
-        data-qa="main-form-save-btn"  
-        colorScheme={isSaved ? '' : 'blue'}
-      >
-        <>{isSaved ? 'Saved!' : 'Save'}</>
-      </Button>
-
-    </form>
+        <Collapse in={isOpen} animateOpacity>
+          <Button 
+            size="xlg"
+            id="submitButton"
+            type="submit"
+            variant={isSaved ? 'text' : 'solid'}
+            isDisabled={!amount}   
+            onClick={handleSpecialCaseClick}
+            width="100%"
+            data-qa="main-form-save-btn"  
+            colorScheme={isSaved ? '' : 'blue'}
+            sx={{ 
+              ...(isSaved && {
+                color: 'green.600' 
+              }),
+              mt: 8,              
+            }}
+          >
+            <>{isSaved ? 'Saved!' : 'Save'}</>
+          </Button>
+        </Collapse>              
+      </form>
+    </Card>
   );
 }

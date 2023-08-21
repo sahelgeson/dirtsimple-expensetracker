1d40c278acc98516b5a2e3787f49033b411ae27b
Scott <sahelgeson07@gmail.com>
2023-08-21 10:10:07 -0400

    makes a pressable HomeForm card that changes focus to amount on initial interaction

--- src/routes/Home/HomeForm.tsx
+++ src/routes/Home/HomeForm.tsx
@@ -9,9 +9,50 @@ import { NumberInput, NumberInputField } from '@chakra-ui/react';
 import { UNCATEGORIZED } from 'lib/constants';
 import { CategoryId, Dollar, EmptyString } from 'interfaces';
 
+const highlightBottom = '0 4px 3px 1px #FCFCFC';
+const shadowBottom = '0 6px 8px #ebebeb';
+const shadowInsetHover = 'inset 0 0 3px 3px #CECFD1';
+const shadowInsetFocus = 'inset 0 0 10px 0px rgba(0, 0, 250, .6)';
+const shadowInsetActive = 'inset 0 0 5px 3px #eee';
+
+const buttonShadowBase = `${highlightBottom}, ${shadowBottom}`;
+const buttonShadowHover = `${buttonShadowBase}, ${shadowInsetHover}`;
+const buttonShadowFocus = `${buttonShadowBase}, ${shadowInsetFocus}`;
+const buttonShadowActive = `${buttonShadowBase}, ${shadowInsetActive}`;
+
+const cardBorderRadius = 110;
+
+const cardPressableSx = {
+  background: 'linear-gradient(to top, #f6f6f6 0%, #fff 60%, #FDFDFD 100%)',
+  boxShadow: '2px 3px 8px 3px rgba(0, 0, 0, 0.05)',
+  padding: 2,
+  borderRadius: cardBorderRadius,
+  border: '1px solid #d3d3d3',
+  transition: 'all .2s ease',
+  '@keyframes active': {
+    '0%': {
+      boxShadow: `${buttonShadowFocus}`,
+    },
+    '100%': {
+      boxShadow: `${buttonShadowHover}`,
+    }
+  },
+  '& > *': {
+    transition: 'transform .2s ease',
+  }, 
+  '&.active': {
+    boxShadow: `${buttonShadowActive}`,
+    '& > *': {
+    },    
+  },    
+}
+
+const clickTapThroughElementIds = ['amount', 'category', 'submitButton'];
+
 export const HomeForm = () => {
   const { allCategories, addExpense } = useGlobalState();
   const { isOpen, onToggle } = useDisclosure();
+  const [isActiveCard, setIsActiveCard] = useState<boolean>(false);
 
   /* TODO: change this eventually so user can set default category */
   //const defaultCategoryId = allCategories[0]?.id  || [];
@@ -43,8 +84,9 @@ export const HomeForm = () => {
     if (isOpen && !isSaved) return true;
     const targetId = (event.target as HTMLElement)?.id; 
 
+    const isClickTapThrough = clickTapThroughElementIds.includes(targetId);
     // reset and focus on amount if opening Card
-    if (targetId !== 'amount' && targetId !== 'category') {
+    if (!isClickTapThrough) {
       if (!isOpen) {
         onToggle();
       } 
@@ -52,6 +94,19 @@ export const HomeForm = () => {
     }
 
     return false;
+  }
+
+  const handleActiveState = (event: React.TouchEvent<HTMLElement>, isActive: boolean) => {
+    // only trigger active state if user isn't already interacting with the form
+    if (isOpen) return true;
+
+    const targetId = (event.target as HTMLElement)?.id; 
+    const isClickTapThrough = clickTapThroughElementIds.includes(targetId);
+    if (!isClickTapThrough) {
+      setIsActiveCard(isActive);
+      return false;
+    }
+    return true;
   }
 
   const handleAmountChange = (valueAsNumber: number) => {
@@ -108,87 +163,104 @@ export const HomeForm = () => {
   }  
 
   return (    
-    <Card p={12} pt={24} pb={isSaved ? 12 : 24} sx={{ borderRadius: 110 }} onClick={handleOpenCollapsible}>
-      <form onSubmit={handleSubmit}>
-        <label 
-          htmlFor="amount"
-          className="sr-only"            
-        >
-          Enter amount of this expense  
-        </label>
-
-        <NumberInput 
-          id="amount"
-          value={amount}
-          onChange={(_, valueAsNumber) => handleAmountChange(valueAsNumber)}
-          onFocus={handleFocus}        
-          size="xlg"
-          variant="darkerBorder"
-          mb={2}
-        >
-          <NumberInputField         
-            width={'100%'} 
-            placeholder="$0.00"
-            data-qa="main-form-amount-input" 
-          />
-        </NumberInput>
-
-        <Box mt={4} mb={2}>
-          <label
-            htmlFor="category"
-            className="block text-center gray-777 mbs"
+    <Card sx={cardPressableSx} 
+      onClick={handleOpenCollapsible}
+      onTouchStart={(e) => handleActiveState(e, true)}
+      onTouchEnd={(e) => handleActiveState(e, false)}
+      className={isActiveCard ? 'active' : ''}
+    >
+      <Box 
+        px={8} 
+        py={24} 
+        sx={{
+          background: 'linear-gradient(to bottom, #f8f8f8 0%, #fff 20%, #FDFDFD 100%)',
+          borderRadius: cardBorderRadius,
+        }}
+      >
+        <form onSubmit={handleSubmit}>
+          <label 
+            htmlFor="amount"
+            className="sr-only"            
           >
-            Category
+            Enter amount of this expense  
           </label>
-        </Box>
 
-        <Select 
-          id="category"
-          placeholder="Select category"
-          size="xlg"
-          variant="darkerBorder"
-          value={categoryId} 
-          onChange={handleCategoryChange}
-          data-qa="main-form-category-input" 
-        >     
-          {allCategories
-            .filter((category) => {
-              return category.id !== UNCATEGORIZED
-            })
-            .map((category) => {
-              return (
-                <option 
-                  key={category.id}
-                  value={category.id}
-                >
-                  {category.name}
-                </option>
-            )}
-          )}
-        </Select>
-
-        <Collapse in={isOpen} animateOpacity>
-          <Button 
+          <NumberInput 
+            id="amount"
+            value={amount}
+            onChange={(_, valueAsNumber) => handleAmountChange(valueAsNumber)}
+            onClick={(e) => e.stopPropagation()}
+            onFocus={handleFocus}        
             size="xlg"
-            id="submitButton"
-            type="submit"
-            variant={isSaved ? 'text' : 'solid'}
-            isDisabled={!amount}   
-            onClick={handleSpecialCaseClick}
-            width="100%"
-            data-qa="main-form-save-btn"  
-            colorScheme={isSaved ? '' : 'blue'}
-            sx={{ 
-              ...(isSaved && {
-                color: 'green.600' 
-              }),
-              mt: 8,              
-            }}
+            variant="darkerBorder"
+            mb={2}
           >
-            <>{isSaved ? 'Saved!' : 'Save'}</>
-          </Button>
-        </Collapse>              
-      </form>
+            <NumberInputField         
+              width={'100%'} 
+              sx={{ backgroundColor: 'white' }}
+              placeholder="$0.00"
+              data-qa="main-form-amount-input" 
+            />
+          </NumberInput>
+
+          <Box mt={4} mb={2}>
+            <label
+              htmlFor="category"
+              className="block text-center gray-777 mbs"
+            >
+              Category
+            </label>
+          </Box>
+
+          <Select 
+            id="category"
+            placeholder="Select category"
+            size="xlg"
+            variant="darkerBorder"
+            value={categoryId} 
+            onChange={handleCategoryChange}
+            onClick={(e) => e.stopPropagation()}
+            data-qa="main-form-category-input" 
+          >     
+            {allCategories
+              .filter((category) => {
+                return category.id !== UNCATEGORIZED
+              })
+              .map((category) => {
+                return (
+                  <option 
+                    key={category.id}
+                    value={category.id}
+                  >
+                    {category.name}
+                  </option>
+              )}
+            )}
+          </Select>
+
+          <Collapse in={isOpen} animateOpacity>
+            <Button 
+              size="xlg"
+              id="submitButton"
+              type="submit"
+              variant={isSaved ? 'text' : 'solid'}
+              isDisabled={!amount}   
+              onClick={handleSpecialCaseClick}
+              width="100%"
+              data-qa="main-form-save-btn"  
+              colorScheme={isSaved ? '' : 'blue'}
+              sx={{ 
+                ...(isSaved && {
+                  color: 'green.600' 
+                }),
+                mt: 8,              
+              }}
+            >
+              <>{isSaved ? 'Saved!' : 'Save'}</>
+            </Button>
+          </Collapse>              
+        </form>
+      </Box>
     </Card>
   );
 }
